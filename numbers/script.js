1e095d0afbe5945049e9e7f2307cd5e8963467b2
Sara S. Hashimova <sarash@code.local>
2023-04-25 18:51:44 +0400

    first commit

---
+++ numbers/script.js
@@ -0,0 +1,64 @@
+// Task 1
+// let existsUserLogin = "the_best_user";
+// let existsUserPassword = 12345678;
+// let userLogin = prompt("Enter Login:").trim();
+// let userPassword = Number(prompt("Enter Password:").trim());
+
+// if(userLogin === existsUserLogin && userPassword === existsUserPassword){
+//     alert(`Welcome ${existsUserLogin}`);
+// }else{
+//     alert("Login or Password entered incorrectly");
+// }
+  
+
+
+// Task 2
+
+// let newStudent = (prompt("Enter the name of the new student:")).trim();
+// let times = 1;
+
+// while(times <= 3 && newStudent) {
+//     alert(`Welcome ${newStudent}`);
+//     times++;
+// }
+
+// Task 3
+
+// let number = 100;
+// let total = 0;
+// for(let i=0; i<=number; i++){
+//     total+=i;
+// }
+// alert(total);
+
+//Task 4
+
+// let question1 = Number(prompt("What's 2+2?"));
+// let answer1 = 4;
+// checkAnswer(question1,answer1);
+// let question2 = Number(prompt("How much will 2 * 2 be?"));
+// let answer2 = 4;
+// checkAnswer(question2,answer2);
+// let question3 = Number(prompt("Petya had 5 apples. He ate 3 of them and gave 1 to a friend. How many apples does Peter have left?"));
+// let answer3 = 1;
+// checkAnswer(question3,answer3);
+// let question4 = Number(prompt("Masha had 10 sweets. She ate 2 and gave 1 to a friend. After that, mom gave Masha 5 more sweets.How many sweets did Masha have left in the end?"));
+// let answer4 = 12;
+// checkAnswer(question4,answer4);
+// let question5 = Number(prompt("What is 2 + 2 * 2?"));
+// let answer5 = 6;
+// checkAnswer(question5,answer5);
+
+// function checkAnswer(studentAnswer, trueAnswer) {
+//     if(studentAnswer === trueAnswer) {
+//         alert("your answer correct");
+//     }else{
+//         alert("your answer incorrect"); 
+//     }
+// }
+
+
+//Task 5
+let clientName = "Igor";
+let clientSpentToday = 25;
+let clientSpentForAllTime;
